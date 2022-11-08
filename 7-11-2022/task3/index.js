5ed2dcd22a757facbd1d7591794c184ef071b736
hassan ayasrah <hassanayasrah68@gmail.com>
2022-11-08 09:14:42 +0300

    updating task 3

--- 7-11-2022/task3/index.js
+++ 7-11-2022/task3/index.js
@@ -1,7 +1,7 @@
-let shortestLength = 100;
-let shortestValue ;
-
 function shorterInArray(arr){
+    let shortestLength = arr[0].length;
+    let shortestValue = arr[0];
+
     for(let i=0; i<arr.length; i++){
         if(arr[i].length < shortestLength){
             shortestLength = arr[i].length;
