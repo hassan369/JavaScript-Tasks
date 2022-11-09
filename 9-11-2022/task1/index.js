4ec2962e832c153dfc2a50f846f76b9da1ef43b4
hassan ayasrah <hassanayasrah68@gmail.com>
2022-11-09 16:58:09 +0300

    uploading task 11-0

---
+++ 9-11-2022/task1/index.js
@@ -0,0 +1,40 @@
+function formSave(){
+    
+    
+
+    let person = {};
+    person.name = document.getElementById('name').value;
+    person.age = document.getElementById('age').value;
+    if(document.getElementById('mail').checked){
+        person.gender = document.getElementById('mail').value;
+    } else if(document.getElementById('femail').checked){
+        person.gender = document.getElementById('femail').value;
+    }
+
+    person.description = document.getElementById('textareaD').value;
+    person.major = document.getElementById('major').value;
+
+    person.languge = "";
+    if (document.getElementById('html').checked){
+        person.languge += document.getElementById('html').value;
+    }  if (document.getElementById('css').checked){
+        person.languge += document.getElementById('css').value;
+    }  if (document.getElementById('js').checked){
+        person.languge += document.getElementById('js').value;
+    }
+
+    
+
+    localStorage.setItem('personObj',JSON.stringify(person));
+    let perosnFormStorage = JSON.parse(localStorage.getItem('personObj')) ;
+    
+    document.getElementById('card').style.display = "block";
+    document.getElementById('nameAfter').innerHTML = perosnFormStorage.name;
+    document.getElementById('ageAfter').innerHTML = perosnFormStorage.age;
+    document.getElementById('genderAfter').innerHTML = perosnFormStorage.gender;
+    document.getElementById('descriptionAfter').innerHTML = perosnFormStorage.description;
+    document.getElementById('majorAfter').innerHTML = perosnFormStorage.major;
+    document.getElementById('preferdAfter').innerHTML = perosnFormStorage.languge;
+    document.getElementById('nameAfter').innerHTML = perosnFormStorage.name;
+}
+
---
+++ 9-11-2022/task2/index.js
@@ -0,0 +1,45 @@
+function formSave(){
+    
+    
+
+    let person = {};
+    person.subject = document.getElementById('subject').value;
+    
+    
+
+    person.description = document.getElementById('textareaD').value;
+    
+
+    person.user = "";
+    if (document.getElementById('admin').checked){
+        person.user += document.getElementById('admin').value;
+    }  if (document.getElementById('visitor').checked){
+        person.user += document.getElementById('visitor').value;
+    }  if (document.getElementById('loged').checked){
+        person.user += document.getElementById('loged').value;
+    }
+
+
+    person.technology = "";
+    if (document.getElementById('html').checked){
+        person.technology += document.getElementById('html').value;
+    }  if (document.getElementById('css').checked){
+        person.technology += document.getElementById('css').value;
+    }  if (document.getElementById('js').checked){
+        person.technology += document.getElementById('js').value;
+    }
+
+    
+
+    localStorage.setItem('subjectObj',JSON.stringify(person));
+    let subjectFromStorage = JSON.parse(localStorage.getItem('subjectObj')) ;
+    
+    document.getElementById('card').style.display = "block";
+
+    document.getElementById('subjectAfter').innerHTML = subjectFromStorage.subject;
+    document.getElementById('descriptionAfter').innerHTML = subjectFromStorage.description;
+    document.getElementById('userAfter').innerHTML = subjectFromStorage.user;
+    document.getElementById('technologyAfter').innerHTML = subjectFromStorage.technology;
+   
+}
+
