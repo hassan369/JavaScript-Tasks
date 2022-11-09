5f8207c61825f02af0589caf6023defb7856534c
hassan ayasrah <hassanayasrah68@gmail.com>
2022-11-09 09:44:06 +0300

    updating task 1

--- 8-11-2022/task1/index.js
+++ 8-11-2022/task1/index.js
@@ -1,7 +1,3 @@
-let pp = document.getElementById('p1');
-pp.style.backgroundColor = "yellow";
-pp.style.width = "300px";
-pp.style.border = "1px solid black";
-pp.style.textAlign = "center";
-pp.style.fontWeight = "bold";
-pp.style.fontSize = "40px";
+
+
+document.getElementById("p11").setAttribute('id','p1');
