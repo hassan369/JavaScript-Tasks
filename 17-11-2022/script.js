0d992ea6ef2947c6d368a36efdf126853ed6f04b
hassan ayasrah <hassanayasrah68@gmail.com>
2022-11-17 12:12:49 +0300

    adding task 17-11

---
+++ 17-11-2022/script.js
@@ -0,0 +1,22 @@
+async function getData(){
+    const res = await fetch("https://www.breakingbadapi.com/api/characters");
+    
+    const data = await res.json();
+    let select = document.getElementById('charecters');
+    for(let i = 0; i<data.length; i++){
+        // document.getElementById("charecters")[0].innerHTML += `<option value="${data[i].img}">${data[i].name}</option>`;
+        let opt = document.createElement('option');
+        opt.value = data[i].img;
+        opt.innerHTML = data[i].name;
+        select.appendChild(opt);
+
+    }
+}
+getData();
+
+
+function changeImg(){
+    let selected = document.getElementById('charecters').value;
+    document.getElementById('charImg').src = selected;
+}
+
